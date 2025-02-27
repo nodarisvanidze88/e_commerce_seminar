63adb76ece514d95d3697db4397514902fbdc610
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2025-02-27 21:50:05 +0400

    add test function

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.http import HttpResponse
+def test(request):
+    return HttpResponse('Hello, World!')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', test)
 ]
